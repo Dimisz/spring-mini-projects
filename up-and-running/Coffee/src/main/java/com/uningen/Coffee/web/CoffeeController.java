96b410930fd312e8cc4a036445d5611cfed67321
Vladimir Solovyov <leilyngale@icloud.com>
2024-01-19 18:56:13 +0800

    Tweaking environment

--- up-and-running/Coffee/src/main/java/com/uningen/Coffee/web/CoffeeController.java
+++ up-and-running/Coffee/src/main/java/com/uningen/Coffee/web/CoffeeController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,13 +15,6 @@ public class CoffeeController {
 
     public CoffeeController(CoffeeRepository coffeeRepository){
         this.coffeeRepository = coffeeRepository;
-
-        this.coffeeRepository.saveAll(List.of(
-                new Coffee("Cafe Cereza"),
-                new Coffee("Cafe Ganador"),
-                new Coffee("Cafe Lareno"),
-                new Coffee("Cafe Tres Pontas")
-        ));
     }
 
     @GetMapping
