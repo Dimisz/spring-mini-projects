0fbe83e9ce3207174245c16a5eafb19e9ee30b2b
Vladimir Solovyov <leilyngale@icloud.com>
2024-01-24 20:54:24 +0800

    Scaffold the Controller

---
+++ up-and-running/MVCAircraft/src/main/java/com/uningen/MVCAircraft/MvcAircraftApplication.java
@@ -0,0 +1,13 @@
+package com.uningen.MVCAircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcAircraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcAircraftApplication.class, args);
+	}
+
+}
