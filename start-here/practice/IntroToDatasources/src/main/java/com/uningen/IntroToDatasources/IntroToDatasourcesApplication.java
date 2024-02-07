c2927ae1c25408bd044a282c7e97ffd4e2f3ce48
Vladimir Solovyov <leilyngale@icloud.com>
2024-02-07 16:59:14 +0800

    Intro to data sources

---
+++ start-here/practice/IntroToDatasources/src/main/java/com/uningen/IntroToDatasources/IntroToDatasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.uningen.IntroToDatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToDatasourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToDatasourcesApplication.class, args);
+	}
+
+}
---
+++ start-here/practice/IntroToDatasources/src/test/java/com/uningen/IntroToDatasources/IntroToDatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uningen.IntroToDatasources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToDatasourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
